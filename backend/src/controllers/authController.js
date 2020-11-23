a12df418e7a999c866173c73e96aac8193bd0732
mhrocha1997 <marcelo.h.rocha1997@gmail.com>
2020-11-23 20:47:40 -0300

    Password encryption and email validation

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,17 +1,27 @@
 const express = require('express');
 
+
 const User = require('../model/user');
 
 const router = express.Router();
 
 router.post('/register', async(req,res)=>{
+    const {email } = req.body;
+    
     try{
+        if (await User.findOne({email}))
+            return res.status(400).send({error: 'User already exists'});
+        
         const user = await User.create(req.body);
+
+        user.password = undefined;
 
         return res.send({user});
     }catch(err){
         return res.status(400).send({error: err});
     }
 });
+
+
 
 module.exports = app =>app.use('/auth',router);
