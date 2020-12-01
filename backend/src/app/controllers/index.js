6044f274b02aa4ad197779afcf69e3184a213176
mhrocha1997 <marcelo.h.rocha1997@gmail.com>
2020-12-01 14:04:35 -0300

    Dirname fixes

--- backend/src/app/controllers/index.js
+++ backend/src/app/controllers/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = app=>{
     fs
-        .readdirSync('__dirname')
+        .readdirSync(__dirname)
         .filter(file=>((file.indexOf('.')) !=0 && (file!== "index.js") ))
         .forEach(file=>require(path.resolve(__dirname,file))(app));
 };
