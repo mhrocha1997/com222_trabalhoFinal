cddc380bd6e2bb5abf4aab3f91405d355d14f428
mhrocha1997 <marcelo.h.rocha1997@gmail.com>
2020-12-01 13:51:28 -0300

    Error on sending email

--- backend/src/app/controllers/authController.js
+++ backend/src/app/controllers/authController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
+const mailer = require('../../modules/mailer');
 
 const authConfig = require('../../config/auth.json');
 
@@ -77,7 +78,17 @@ router.post("/forgot_password",async(req,res)=>{
             }
         });
 
-        console.log(token,now);
+        mailer.sendMail({
+            to: email,
+            from: 'marcelo.h.rocha1997@gmail.com',
+            template: 'auth/forgot_password',
+            context: {token},
+        }, (err)=>{
+            console.log(err);
+            if(err)
+                return res.status(400).send({error: 'Cannot send forgot password email'});
+            return res.send();
+        });
 
     }catch{
         res.status(400).send({error: 'Error on forgot password, try again'});
--- backend/src/modules/mailer.js
+++ backend/src/modules/mailer.js
@@ -1,5 +1,5 @@
 const nodemailer = require('nodemailer');
-const hbs = require("nodemailer-express-handlebars");
+const hbs = require('nodemailer-express-handlebars');
 const path = require('path');
 
 const{ host,port,user,pass} = require('../config/mail.json');
