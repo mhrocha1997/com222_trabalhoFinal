1f0aa05c97c79da52259e17d629bb98d9d0d7ca6
mhrocha1997 <marcelo.h.rocha1997@gmail.com>
2020-12-06 18:49:38 -0300

    Cors instalation

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes');
 const app = express();
+const cors = require('cors');
 
 app.use('/uploads',express.static(path.join(__dirname,'..','..','uploads')));
 
 app.use(express.json());
 app.use(routes);
-
+app.use(cors());
 app.listen(3000);
 
 module.exports = app;
