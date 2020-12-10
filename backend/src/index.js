5bab96bb046241d890379d6dcd000a06e95a90f9
LeoGonzaga <sacredlotus05@gmail.com>
2020-12-09 22:03:27 -0300

    subindo quase a ultima versao

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const path = require('path');
-const routes = require('./routes');
-const cors = require('cors');
-
+const express = require("express");
+const path = require("path");
+const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
-app.use('/uploads',express.static(path.join(__dirname,'..','..','uploads')));
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "..", "..", "uploads"))
+);
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Servidor online");
+});
 
 module.exports = app;
